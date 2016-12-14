db17ba1b2d9a6e7acef484f9ce595f827c28a5d1
blagoja ovezoski <blagojaovezoski@gmail.com>
2016-12-14 14:22:31 +0100

    after vide

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ var server = http.createServer(function(request, response){
   response.writeHead(200, {"Content-Type": "text/html"});
   response.end(app(request));
 });
-server.listen(process.env.PORT || 8080, function(){
-  console.log('listening on', http.address().port);
-});
+server.listen(process.env.PORT || 8080);
 console.log("Server running at http://localhost:8080/");
